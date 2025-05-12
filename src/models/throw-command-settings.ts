075f810ba7b57909eca2ce0273604ac347171da0
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-05-12 19:28:44 -0400

    Renamed throw command settings schema file
    Renamed file to be more accurate.
    
    Additionally, added 'enabled' property to the throw command

--- src/models/throw-command-settings.ts
+++ src/models/throw-command-settings.ts
@@ -3,6 +3,7 @@ import { ThrowSettings } from "../classes/command-settings.js";
 
 const ThrowConfig = new Schema<ThrowSettings>({
 	guildId: { type: String, required: true, unique: true },
+	enabled: { type: Boolean, default: true },
 	customItems: { type: [String], default: [] },
 	cooldown: { type: Number, default: null },
 	customItemsOnly: { type: Boolean, default: false },
