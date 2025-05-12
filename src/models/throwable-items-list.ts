05b3982b57aa56c83db28d1349b3c20444199809
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-05-11 21:26:13 -0400

    Added Throw Command Settings Schema

---
+++ src/models/throwable-items-list.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from "mongoose";
+import { ThrowSettings } from "../classes/command-settings.js";
+
+const ThrowConfig = new Schema<ThrowSettings>({
+	guildId: { type: String, required: true, unique: true },
+	customItems: { type: [String], default: [] },
+	cooldown: { type: Number, default: null },
+	customItemsOnly: { type: Boolean, default: false },
+	blacklistedChannels: { type: [String], default: [] },
+});
+
+export default model(`Throw Command Settings`, ThrowConfig);
