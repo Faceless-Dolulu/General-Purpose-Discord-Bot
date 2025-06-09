7887fe9266a5b2fa87eaac466c354e4b48290980
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-06-08 21:22:25 -0400

    Updated command settings cache to auto-log # of expired entries cleared

--- src/util/command-settings-cache.ts
+++ src/util/command-settings-cache.ts
@@ -1,9 +1,25 @@
 import NodeCache from "node-cache";
+import { clearTimeout } from "timers";
 
 export type CommandSettingsKey = `${string}:${string}`;
 
 export const commandSettingsCache = new NodeCache({
-	stdTTL: 300,
+	stdTTL: 60,
 	checkperiod: 60,
-	useClones: false,
+	useClones: true,
+});
+let expiredWaveCount = 0;
+let expiredWaveTimer: NodeJS.Timeout | null = null;
+
+commandSettingsCache.on("expired", () => {
+	expiredWaveCount++;
+
+	if (expiredWaveTimer) clearTimeout(expiredWaveTimer);
+
+	expiredWaveTimer = setTimeout(() => {
+		console.info(
+			`\x1b[36m[GC]\x1b[0m ${expiredWaveCount} command config(s) have been cleared from the cache.`
+		);
+		expiredWaveCount = 0;
+	}, 1000);
 });
