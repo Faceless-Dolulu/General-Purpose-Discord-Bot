842799905bdd05693080756b182cb5bc8a290b4b
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-06-14 15:03:07 -0400

    Added mute settings menu to menu router system

--- src/commands/guild_configuration/moderation_menus/menu-router.ts
+++ src/commands/guild_configuration/moderation_menus/menu-router.ts
@@ -1,9 +1,11 @@
 import { MessageComponentInteraction } from "discord.js";
 import { timeoutSettingsMenu } from "./timeout-settings-menu.js";
+import { muteSettingsMenu } from "./mute-settings-menu.js";
 
 export const moderationMenuHandlers: Record<
 	string,
 	(interaction: MessageComponentInteraction) => Promise<void>
 > = {
 	"timeout-command": timeoutSettingsMenu,
+	"mute-command": muteSettingsMenu,
 };
